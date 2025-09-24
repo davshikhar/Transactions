0e827404065ecffece34db2b94d796fb01bce2c9
davshikhar <shikharyadav900@gmail.com>
2025-09-24 22:35:47 +0530

    Added db and initial routes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const app = express();
 
+app.use(express.json());
+const cors = require('cors');
+app.use(corse());
+
+app.post('/singup', (req,res)=>{
+    //allows the user to sign up as new user
+});
+
+app.post('/signin', (req,res)=>{
+    //allows the user to sign in 
+});
+
+app.get('/user',(req,res)=>{
+    //returns the user data
+})
 
