cba4db62f7f0237587bffd25f64c7ce5217613c5
davshikhar <shikharyadav900@gmail.com>
2025-09-25 22:39:32 +0530

    Added the update endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,21 +2,24 @@ const express = require("express");
 const app = express();
 const z = require('zod');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 app.use(express.json());
 const cors = require('cors');
 const { parse } = require("@typescript-eslint/typescript-estree");
 app.use(corse());
 
-app.post('/singup', (req,res)=>{
-    //allows the user to sign up as new user
-    const requiredBody = z.object({
+const secret = "random123"
+
+const requiredBody = z.object({
         firstname:z.string().max(20),
         lastname:z.string().maz(20),
         email:z.string().email(),
         password:z.string().max(50).regex(/[A-Z]/,{message:"password must contain at least one uppercase letter"})
     });
 
+app.post('/singup', async (req,res)=>{
+    //allows the user to sign up as new user
     const {firstname, lastname, email, password} = req.body;
     const parsedDataWithSuccess = requiredBody.safeParse({firstname, lastname, email, password});
     if(!parsedDataWithSuccess.success){
@@ -26,18 +29,81 @@ app.post('/singup', (req,res)=>{
     }
 
     const hashedPassword = bcrypt.hash(password, 3);
-    //store the user in the database along with the hashed password
-    return res.status(201).json({
-        message:"User created successfully"
-    });
-    
+    //store the user in the database along with the hashed password first check if any error comes or not!
+    let errorThrown = false;
+    try{
+        await UserModel.create({
+            firstname:firstname,
+            lastname:lastname,
+            email:email,
+            password:hashedPassword
+        });
+    }
+    catch(e){
+        res.json({
+            message:"Error creating user in the database"
+        });
+        errorThrown = true;
+    }
+
+    if(!errorThrown){
+        return res.status(201).json({
+            message:"User created successfully"
+        });
+    }
 });
 
-app.post('/signin', (req,res)=>{
+app.post('/signin', async (req,res)=>{
     //allows the user to sign in 
+    const {email,password} = req.body;
+    const parsedDataWithSuccess = requiredBody.safeParse({email,password});
+    if(!parsedDataWithSuccess.success){
+        res.json({
+            message:parsedDataWithSuccess.error
+        });
+    }
+
+    const user = await UserModel.find({email:email});
+
+    if(!user){
+        return res.status(400).json({
+            message:"User not found in the database"
+        });
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if(!passwordMatch){
+        res.status(400).json({message:"Password does not match"});
+    }
+
+    const token = jwt.sign({id:user._id.toString()},secret);
+    return res.status(200).json({
+        message:"User signed in successfully",
+        token:token
+    });
 });
 
-app.get('/user',(req,res)=>{
-    //returns the user data
+app.put('/update',async (req,res)=>{
+    //endpoint to update the user details
+    const {token, firstname, lastname, password} = req.body;
+    if(!token){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+    const verify = jwt.verify(token, secret);
+    if(!verify){
+        return res.status(401).json({message:"Unauthorized"});
+    }
+
+    const userId = verify.id;
+    try{
+        await UserModel.findByIdAndUpdate(userId,{
+            firstname:firstname,
+            lastname:lastname,
+            password:password});
+            return res.status(200).json({message:"User updated successfully"});
+    }
+    catch(e){
+        return res.status(500).json({message:"Error updating user", error:e});
+    }
 })
 
